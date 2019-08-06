ad3bb0107afbe5df7a58c2bc2198c47ca10cdcd5
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-06 13:37:48 +0530

    Create Quadratic.java

---
+++ Quadratic.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.functional;
+import java.util.Scanner;
+public class Quadratic {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Co-ordinates of quadratic equation");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		float d=b*b-4*a*c;
+		double root1=(-b+Math.sqrt(d))/(2*a);
+		double root2=(-b-Math.sqrt(d))/(2*a);
+		System.out.println("Roots of Quadratic Equation is:"+root1+"and"+root2);
+	}
+
+}
