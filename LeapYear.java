23bb0abfbbc589f3436315dc6b9251212c7e50bd
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-07 11:28:45 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,20 +1,32 @@
-package com.bridgelabz.functional;
+/*  Purpose: To Determines whether the year is leap year or not
+ *
+ *  @author  Ravi Maurya
+ *  @version 1.0
+ *  @since   07-08-2019
+ *
+ ******************************************************************************/
+package com.BridgeLabz.functional;
 import java.util.*;
 public class LeapYear {
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the year:");
-		int Y=sc.nextInt();
-		if(Y>=1582)
+		public static void main(String[] args)
 		{
-			if((Y%4==0 && Y%100!=0)|| Y%400==0)
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("Enter the year:");
+			int Year=scanner.nextInt();
+			if(Year>=1582)
 			{
-				System.out.println("Year is ")
-				
+				if((Year%4==0 && Year%100!=0) || Year%400==0)
+				{
+					System.out.println("Year is a Leap Year");
+				}
+				else
+					System.out.print("Year is not a Leap Year");
 			}
-			                     
+				else
+					System.out.print("Year is not a Leap Year");
+			scanner.close();
+		
 		}
+
 	}
 
-}
