d6f1989d7d4c08cb32d59365b0810d1fdd79fa7a
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-07 09:29:22 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.functional;
+import java.util.*;
+public class LeapYear {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the year:");
+		int Y=sc.nextInt();
+		if(Y>=1582)
+		{
+			if((Y%4==0 && Y%100!=0)|| Y%400==0)
+			{
+				System.out.println("Year is ")
+				
+			}
+			                     
+		}
+	}
+
+}
