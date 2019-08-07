5a8a74b7d9a47e0228760928e69762c08621107a
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-07 11:43:48 +0530

    Update PrintThreeNames.java

--- PrintThreeNames.java
+++ PrintThreeNames.java
@@ -1,24 +1,28 @@
+
 /*  Purpose: Print the names in reverse order
- *
- *  @author  Ravi Maurya
- *  @version 1.0
- *  @since   06-08-2019
- *
- ******************************************************************************/
-package com.bridgelabz.functional;
+		 *
+		 *  @author  Ravi Maurya
+		 *  @version 1.0
+		 *  @since   06-08-2019
+		 *
+		 *****************************************************************************/
+package com.BridgeLabz.functional;
 import java.util.Scanner;
-public class PrintThreeNames {
-public static void main(String[] args) {
-	
-	Scanner scanner=new Scanner(System.in);
-	System.out.println("java Print Three Names");
-	String string=scanner.nextLine();
-	String string2[]=new String[5];
-			s=string.split(" ");
-	System.out.print("Hi"+" ");
-	for(int i=string2.length-1;i>=0; i--)
-	{
-		System.out.print(string2[i]+","+" ");
-	} 
-  }
+public class PrintThreeNames 
+{
+
+	public static void main(String[] args) 
+	{		
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("java Print Three Names");
+			String string=scanner.nextLine();
+			String string2[]=new String[5];
+					string2=string.split(" ");
+			System.out.print("Hi"+" ");
+			for(int i=string2.length-1;i>0; i--)
+			{
+				System.out.print(string2[i]+","+ string2[0]);
+			} 
+			scanner.close();
+	}
 }
