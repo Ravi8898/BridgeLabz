626a43c86c8ef038c49c0245ce88c044a601d428
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-08 11:30:41 +0530

    Create DoubleOpt.java

---
+++ DoubleOpt.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.functional;
+
+import java.util.Scanner;
+
+import com.utility.Utility;
+
+public class DoubleOpt {
+
+	public static void main(String[] args) 
+	{
+		double a,b,c;
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.println("Enter the value of a:");
+		a=scanner.nextDouble();
+		System.out.println("Enter the value of b:");
+		b=scanner.nextDouble();
+		System.out.println("Enter the value of c:");
+		c=scanner.nextDouble();
+		
+		System.out.println("Please select the operation which you want to perform \n 1. a+(b*c) \n 2. (a*b)+c \n 3. (a%b)+c");
+		int select=scanner.nextInt();
+		
+		Utility utility=new Utility();
+		if(select==1)
+		{
+			double Result=utility.doubleOperation1(a,b,c);
+			System.out.println("Result of the operation is "+Result);
+		}
+		if(select==2)
+		{
+			double Result=utility.doubleOperation2(a,b,c);
+			System.out.println("Result of the operation is "+Result);
+		}
+		if(select==3)
+		{
+			double Result=utility.doubleOperation3(a,b,c);
+			System.out.println("Result of the operation is "+Result);
+		}
+		scanner.close();		
+	}
+}
