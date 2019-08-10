50292f50bd3158b809f52eff0b531503ef360626
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-10 21:05:01 +0530

    Create ArmstrongNumber.java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.functional;
+import java.util.*;
+public class ArmstrongNumber {
+
+	public static void main(String[] args) 
+	{
+		Scanner scanner=new Scanner(System.in);
+		int number, originalNum, numSize, rem=0, sum=0;
+		System.out.println("Enter the no. to check Armstrong no.: ");
+		String string=new String();
+		string=scanner.nextLine();
+		numSize=string.length();
+		number=Integer.parseInt(string);
+		originalNum=number;
+		while(number!=0)
+		{
+			rem=number%10;
+			sum=sum+(int)(Math.pow(rem, numSize));
+			number=number/10;
+		}
+		if(sum==originalNum)
+		
+			System.out.println("The "+originalNum+" is Armstrong number");
+		else
+			System.out.println("The "+originalNum+" is not Armstrong number");
+		scanner.close();
+	}
+
+}
