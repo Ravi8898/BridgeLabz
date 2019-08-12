6f72e8f40c85513470ab59058c199100d0403772
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-13 00:15:57 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,54 +1,32 @@
-/*  Purpose: To Find whether two given Strings are Anagram or not
+/* *****************************************************************************
+ *  Purpose: To Find whether two given Strings are Anagram or not
  *
- *  @author  Ravi Maurya
+ *  @author  Sunny Maurya and Ravi maurya
  *  @version 1.0
- *  @since   10-08-2019
+ *  @since   12-08-2019
  *
  ******************************************************************************/
+
 package com.bridgelabz.functional;
-import java.util.*;
-public class Anagram {
 
-	public static void main(String[] args) 
-	{
+import java.util.Scanner;
+
+public class AnagramNew {
+
+	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter the First String:");
-		String firstStr=scanner.nextLine();
-		String input1=firstStr.toLowerCase();
-		String firstInput=input1.replace(" ", "");
-		
-		System.out.println("Enter the Second String:");
-		String secondStr=scanner.nextLine();
-		String input2=secondStr.toLowerCase();
-		String secondInput=input2.replace(" ", "");
-		
-		int strSize1=firstInput.length();
-		int strSize2=secondInput.length();
-		int varifyAnam = 0;
-		if(strSize1==strSize2)
+		System.out.println("Enter the 1st String");
+		String str1=scanner.nextLine();
+		System.out.println("Enter the 2nd String");
+		String str2=scanner.nextLine();
+		Utility utility=new Utility();
+		boolean b=utility.checkAnagram(str1,str2);
+		if(b==true)
 		{
-			for(int i=0; i<strSize1; i++)
-			{
-			
-				for(int j=0; j<strSize2; j++)
-				{
-					if(firstInput.charAt(i)==secondInput.charAt(j))
-					{
-						varifyAnam++;
-					}
-				}
-			}
-			
+			System.out.println("Both strings are Anagram");
 		}
-		
-		if(varifyAnam==strSize1 && varifyAnam==strSize2)
-			System.out.println("Both Strings are Anagram");
-		
 		else
-			System.out.println("Both Strings are not Anagram");
-		
+			System.out.println("Both strings are not Anagram");
 		scanner.close();
-		
 	}
-
 }
