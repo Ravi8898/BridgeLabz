75d279e42d6ef47690b2b9364fbf97daf3f0a577
Ravi8898 <53813535+Ravi8898@users.noreply.github.com>
2019-08-21 00:13:55 +0530

    Update NoOfSquaresInMatrix.java

--- NoOfSquaresInMatrix.java
+++ NoOfSquaresInMatrix.java
@@ -1,5 +1,5 @@
 /* *****************************************************************************
- *  Purpose: Find the no of Squares in n*n matrix
+ *  Purpose: Find the no of Squares in m*n matrix
  *
  *  @author  Sunny Maurya and Ravi maurya
  *  @version 1.0
@@ -17,22 +17,24 @@ public class NoOfSquaresInMatrix {
 		
 		int sum=0;
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the value of m: ");
+		int m=scanner.nextInt();
 		System.out.println("Enter the value of n: ");
 		int n=scanner.nextInt();
-		System.out.print("No of Squares in "+n+"*"+n+" matrix are: ");
-		int count=countSquares(n,sum);
+		System.out.print("No of Squares in "+m+"*"+n+" matrix are: ");
+		int count=countSquares(m,n,sum);
 		System.out.println(count);
 		scanner.close();
 		
 	}
 	
-	public static int countSquares(int n,int sum) {
+	public static int countSquares(int m,int n,int sum) {
 		
-		if(n==0) 
+		if(n==0 || m==0) 
 		return sum;
 		
-		sum=sum+(n*n);
-		return countSquares(n-1,sum);
+		sum=sum+(m*n);
+		return countSquares(m-1,n-1,sum);
 	}
 
 }
